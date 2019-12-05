7a1d39c5891754ac0ffd4a40d8038879456256b6
bkoch <kochbt1@gmail.com>
2019-12-05 16:24:38 -0600

    Method changes

--- Email.java
+++ Email.java
@@ -1,38 +1,37 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class Email{
-    String firstName;
-    String lastName;
-    String department;
-    String company;
-    int mbCapacity;
-    String password;
-    String altEmail;
+    private String firstName;
+    private String lastName;
+    private String department;
+    private String company;
+    private int mbCapacity;
+    private int defaultPasswordLength = 10;
+    private String password;
+    private String altEmail;
+    private String emailAddress;
     
-    /**
-     * @param company the company to set
-     */
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public void setFirstName(String firstName) {
+    //Constructor
+    public Email(String firstName, String lastName){
         this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
         this.lastName = lastName;
+        this.department = getDepartment();
+        //System.out.println("Department: " + this.department);
+        this.company = getCompany();
+        this.emailAddress = generateEmail(this.firstName, this.lastName, this.department, this.company);
+        System.out.println("Email Created: " + this.emailAddress);
+        this.password = generatePassword(defaultPasswordLength);
+        System.out.println("Your password is: " + this.password);
+        
     }
 
+    //Methods to change the Password, Mailbox Capacity, and Alternate Email
     public void setMbCapacity(int mbCapacity) {
         this.mbCapacity = mbCapacity;
     }
 
-    public void setPassword(String password) {
+    public void changePassword(String password) {
         this.password = password;
     }
 
@@ -40,36 +39,71 @@ class Email{
         this.altEmail = altEmail;
     }
 
-    /**
-     * @return the company
-     */
-    public String getCompany() {
-        return company;
+    //Methods to display the Name, Email, and Mailbox Capacity
+    public String displayName(){
+        return this.firstName + " " + this.lastname;
     }
 
-    public String getDepartment() {
-        return department;
+    public String displayEmail(){
+        return this.emailAddress;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String displayMbCapacity(){
+        return this.mbCapacity;
     }
 
-    public String getLastName() {
-        return lastName;
+    //Retrieve Company name from User
+    private String getCompany() {
+        System.out.println("Enter Company Name: ");
+        Scanner in = new Scanner(System.in);
+        String companyChoice = in.next();
+        in.close();
+        return companyChoice;
+    }
+
+    //Retrieve Department from User
+    private String getDepartment() {
+        System.out.println("Enter department code: 0 - None, 1 - Sales, 2 - Development, 3 - Accounting");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if(depChoice == 0){
+            return "None";
+        }else if(depChoice == 1){
+            return "Sales";
+        }else if(depChoice == 2){
+            return "Development";
+        }else if(depChoice == 3){
+            return "Accounting";
+        }
+        in.close();
+        return "Not a valid department";
+        
+    }
+
+    //Generates random password string of length 10 from the passwordSet characters
+    private String generatePassword(int defaultPasswordLength){
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$";
+        char[] password = new char[defaultPasswordLength];
+        for (int i=0; i<defaultPasswordLength; i++){
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
+
+    //Takes the name, department, and company and forms an email address string
+    private String generateEmail(String firstName, String lastName, String department, String company){
+        String emailAddress;
+        if(department=="None"){   
+            emailAddress = firstName + "." + lastName + "@" + company + ".com";
+        }else{
+            emailAddress = firstName + "." + lastName + "@" + department + "." + company + ".com";
+        }
+        return emailAddress.toLowerCase();
     }
 
     public int getMbCapacity() {
         return mbCapacity;
-    }
-
-    public String generatePassword(){
-        return ;
-    }
-
-    public String generateEmail(String firstName, String lastName, String department, String company){
-        String emailAddress = firstName + "." + lastName + "@" + department + "." + company + ".com";
-        return emailAddress;
     }
 }
 
--- EmailApp.java
+++ EmailApp.java
@@ -1,5 +1,5 @@
 class Main{
     public static void main(String[] args) {
-        System.out.println("hello, world");
+        Email myEmail = new Email("Ben","Koch");
     }
 }
